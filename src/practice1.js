c428cf4533d9ee4b570b393a127d7a73651328c6
twodogbeta <qijinhaoup@163.com>
2020-09-11 20:59:40 +0800

    [qijinhao]complete

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,17 @@
-const parseData
-export { parseData };
+const parseData = (input) => {
+    let {data, column} = input
+    const resultList = [];
+    let dataCount = 0;
+    data.forEach(data => {
+        let newObj = {};
+        let dataCount = 0;
+        data.forEach(dataElement => {
+            newObj[column[dataCount].name] = dataElement;
+            dataCount++;
+        })
+        resultList.push(newObj);
+    });
+    return resultList;
+}
+
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,15 @@
-const inject
+const inject = (items, sections) => {
+    const result = []
+    items.forEach((item, arrayIndex) => {
+        for (let section of sections) {
+            const {content, index} = section
+            if (index === arrayIndex) {
+                result.push(content)
+                break;
+            }
+        }
+        result.push(item)
+    })
+    return result
+}
 export { inject };
